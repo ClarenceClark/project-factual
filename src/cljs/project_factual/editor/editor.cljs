224067cf01c916e3cb81be9cb714bc6b405218d5
Isaac Khor <services@isaackhor.com>
2016-11-06 20:04:43 +0800

    Refactor toolbar into new file

--- src/cljs/project_factual/editor/editor.cljs
+++ src/cljs/project_factual/editor/editor.cljs
@@ -6,7 +6,8 @@
             [cljsjs.codemirror.addon.edit.continuelist]
             [cljsjs.codemirror.addon.edit.closebrackets]
             [cljsjs.codemirror.addon.edit.matchbrackets]
-            [cljsjs.codemirror.mode.markdown]))
+            [cljsjs.codemirror.mode.markdown]
+            [project-factual.views.toolbar :as toolbar]))
 
 (defn- codemirror-component []
   (reagent/create-class
@@ -27,49 +28,8 @@
      (fn [this]
        (r/dispatch [:destroy-editor]))}))
 
-(defn groupbar-elem [group]
-  [:div {:class "dropdown-container groupbar-elem hover-background"
-         :on-click #(r/dispatch [:click-tag group])
-         :on-context-menu #(r/dispatch [:remove-group-from-active group])}
-   (:group.name group)])
-
-(defn new-group []
-  (let [suggestions (r/subscribe [:groupbar-suggestions])
-        sug-active (r/subscribe [:groupbar-suggestions-active])]
-    (fn new-group []
-      [:div.new-group
-       [:input#group-input {:on-change #(r/dispatch [:groupbar-search-change (.-value (.-target %))])
-                            :on-blur #(r/dispatch [:groupbar-suggestions-active false])
-                            :on-focus #(r/dispatch [:groupbar-suggestions-active true])
-                            ; Sync because events gets recycled by react, and we must do
-                            ; stuff with it before that happens. It only dispatches more events, so
-                            ; the fact that it is out of order shouldn't matter, all other events
-                            ; in queue will be processed before the ones dispatched by this one
-                            :on-key-down #(r/dispatch-sync [:groupbar-input %])}]
-       [:div {:class (str "suggestions"
-                          #_(when-not @sug-active " hide"))}
-        (for [sug @suggestions]
-          [:div.suggestion (:group.name sug)])]])))
-
-
-(defn groupbar []
-  (let [groups (r/subscribe [:active-groups])]
-    [:div.groupbar
-     (for [group @groups]
-       ^{:key group}
-       [groupbar-elem group])
-     [new-group]]))
-
 (defn editor []
   [:div {:class "content"}
-   [:div.toolbar
-    [groupbar]
-    [:div.icon-bar
-     [:div {:class "toolbar-icon hover-background icon-info-circled-alt"
-            :on-click #(r/dispatch [:display-active-item-info])}]
-     [:div {:class "toolbar-icon hover-background icon-trash-empty"
-            :on-click #(r/dispatch [:move-active-item-to-trash])}]
-     [:div {:class "toolbar-icon hover-background icon-dot-3"
-            :on-click #(r/dispatch [:menu-extra-toggle])}]]]
+   [toolbar/toolbar]
    [:div.editor
     [codemirror-component]]])
---
+++ src/cljs/project_factual/views/toolbar.cljs
@@ -0,0 +1,48 @@
+(ns project-factual.views.toolbar
+  (:require [re-frame.core :as r]))
+
+(defn groupbar-elem [group]
+  [:div {:class "dropdown-container groupbar-elem hover-background"
+         :on-click #(r/dispatch [:click-tag group])
+         :on-context-menu #(r/dispatch [:remove-group-from-active group])}
+   (:group.name group)])
+
+(defn new-group []
+  (let [suggestions (r/subscribe [:groupbar-suggestions])
+        sug-active (r/subscribe [:groupbar-suggestions-active])]
+    (fn new-group []
+      [:div.new-group
+       [:input#group-input {:on-change #(r/dispatch [:groupbar-search-change (.-value (.-target %))])
+                            :on-blur #(r/dispatch [:groupbar-suggestions-active false])
+                            :on-focus #(r/dispatch [:groupbar-suggestions-active true])
+                            ; Sync because events gets recycled by react, and we must do
+                            ; stuff with it before that happens. It only dispatches more events, so
+                            ; the fact that it is out of order shouldn't matter, all other events
+                            ; in queue will be processed before the ones dispatched by this one
+                            :on-key-down #(r/dispatch-sync [:groupbar-input %])}]
+       [:div {:class (str "suggestions"
+                          (when-not @sug-active " hide"))}
+        (for [sug @suggestions]
+          ^{:key sug}
+          [:div {:class "suggestion"
+                 :on-click #(r/dispatch [:add-group-to-active-item sug])}
+           (:group.name sug)])]])))
+
+(defn groupbar []
+  (let [groups (r/subscribe [:active-groups])]
+    [:div.groupbar
+     (for [group @groups]
+       ^{:key group}
+       [groupbar-elem group])
+     [new-group]]))
+
+(defn toolbar []
+  [:div.toolbar
+   [groupbar]
+   [:div.icon-bar
+    [:div {:class "toolbar-icon hover-background icon-info-circled-alt"
+           :on-click #(r/dispatch [:display-active-item-info])}]
+    [:div {:class "toolbar-icon hover-background icon-trash-empty"
+           :on-click #(r/dispatch [:move-active-item-to-trash])}]
+    [:div {:class "toolbar-icon hover-background icon-dot-3"
+           :on-click #(r/dispatch [:menu-extra-toggle])}]]])
